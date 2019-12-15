f21241b2f84078ffbfdc0a060cf79b4b34dff02b
Shashi Shekhar <shashi20008@gmail.com>
2019-12-15 14:55:48 +0530

    Enable saving a ToDo item

--- client/components/website.jsx
+++ client/components/website.jsx
@@ -5,7 +5,7 @@ import ToDo from './todo';
 import '../css/website.css';
 
 function mapTodosToComponent(todoObj) {
-  return <ToDo text={todoObj.text} />;
+  return <ToDo key={todoObj._id} text={todoObj.text} />;
 }
 
 // sample map function 
@@ -20,15 +20,52 @@ function mapTodosToComponent(todoObj) {
 
 function ToDoWebsite (props) {
   const [todos, updateTodos] = useState([]);
+  const [todoText, updateTodoText] = useState('');
 
-  useEffect(async () => {
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('/todo', {
+        method: 'GET',
+        credentials: 'same-origin'
+      });
+      const responseObj = await res.json();
+      updateTodos(responseObj.todos);
+    })();
+  }, []);
+
+  const saveTodo = async e => {
+    if(todoText === '') {
+      return;
+    }
+
     const res = await fetch('/todo', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'content-type': 'application/json',
+        accept: 'application/json'
+      },
+      body: JSON.stringify({
+        todoText
+      })
+    });
+    const resBody = await res.json();
+    // updateTodos(resBody.todos);
+
+    if(resBody.success === false) {
+      console.log('Operation failed!');
+      return;
+    }
+
+    updateTodoText('');
+
+    const getRes = await fetch('/todo', {
       method: 'GET',
       credentials: 'same-origin'
     });
-    const responseObj = await res.json();
+    const responseObj = await getRes.json();
     updateTodos(responseObj.todos);
-  }, []);
+  };
 
   return (
     <div>
@@ -36,10 +73,14 @@ function ToDoWebsite (props) {
       <div className="container todo-website">
         <div className="row new-todo-container">
           <div className="col-9">
-            <input className="form-control" placeholder="Enter a todo to save" />
+            <input 
+              className="form-control" 
+              placeholder="Enter a todo to save" 
+              value={todoText}
+              onChange={e => updateTodoText(e.target.value)} />
           </div>
           <div className="col-3">
-            <button className="btn btn-primary">Save ToDo</button>
+            <button className="btn btn-primary" onClick={saveTodo}>Save ToDo</button>
           </div>
         </div>
         <div className="row">
--- routes/todo.js
+++ routes/todo.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const uuidv4 = require('uuid/v4');
 
 const TODOS = [{
+  _id: uuidv4(),
   text: 'Call Plumber'
 }, {
+  _id: uuidv4(),
   text: 'Pay bills'
 }];
 
 router.get('/', function(req, res) {
   res.json({ todos: TODOS });
+});
+
+router.post('/', function(req, res) {
+  const text = req.body.todoText;
+  TODOS.push({
+    _id: uuidv4(),
+    text
+  });
+  res.json({ sucess: true });
 });
 
 module.exports = router;
